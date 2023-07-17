d62be90fbc2dfe2a1cd2fe6c42aa605cc66dacf6
AlexandreGarcia-a22004278 <a22004278@alunos.ulht.pt>
2023-07-17 01:17:14 +0100

    Correções

--- stats.py
+++ stats.py
@@ -8,25 +8,34 @@ stats = Blueprint('stats', __name__)
 @stats.route("/stats/unidades/media/total", methods=["GET"])
 def media_unidades_total():
     # Média de presenças de todas as aulas por unidade curricular
-    results = [{'unidade': unidade.nome,
-                'media': round(db.session.query(func.coalesce(func.avg(Aula.presencas), 0.0)).filter_by(
-                    unidade=unidade).scalar(), 2)}
-               for unidade in Unidade.query.all()]
+    results = [{'unidade': unidade,
+                'media': round(media, 2)}
+               for unidade, media
+               in db.session.query(Unidade.nome, func.coalesce(func.avg(Presenca.id), 0.0))
+               .join(Presenca)
+               .join(Aula)
+               .group_by(Unidade.nome).all()]
     return jsonify(results=results), 200
 
 
 @stats.route("/stats/unidades/media/<int:professor_id>", methods=["GET"])
 def media_unidades_by_professor(professor_id):
     # Média de presenças de todas as aulas por unidade curricular associada ao professor
-    results = [{'unidade': unidade.nome,
-                'media': round(db.session.query(func.coalesce(func.avg(Aula.presencas), 0.0)).filter_by(
-                    unidade=unidade).scalar(), 2)}
-               for unidade in Professor.query.get(professor_id).unidades]
+    results = [{'unidade': unidade,
+                'media': round(media, 2)}
+               for unidade, media
+               in db.session.query(Unidade.nome, func.coalesce(func.avg(Presenca.id), 0.0))
+               .join(Presenca)
+               .join(Aula)
+               .filter_by(professor_id=professor_id)
+               .group_by(Unidade.nome).all()]
     return jsonify(results=results), 200
 
+
 @stats.route("/stats/presencas", methods=["GET"])
-def presenca_by_unidade():
-    if not request.args or not request.args.get('unidade_id') or not request.args.get('professor_id'):
+def presencas_by_unidade():
+    if not request.args or not request.args.get('unidade_id', type=int) or not request.args.get('professor_id',
+                                                                                                type=int):
         return jsonify(error="Falta parâmetros para completar o processo!"), 400
 
     try:
@@ -38,13 +47,15 @@ def presenca_by_unidade():
     unidade = Unidade.query.get(unidade_id)
     professor = Professor.query.get(professor_id)
 
-    if not unidade or unidade not in professor.unidades:
+    if not unidade or not professor or unidade not in professor.unidades:
         return jsonify(error="Unidade não existe ou não está associada ao professor!"), 400
 
+    # Número de presenças de todas as aulas por unidade curricular associada ao professor
     results = [{'numero': aluno_id,
                 'presencas': presencas}
                for aluno_id, presencas
-               in db.session.query(Presenca.aluno_id, func.sum(Aula.presencas)).join(Aula).filter(
-            Aula.unidade_id == unidade_id).group_by(Presenca.aluno_id).all()]
-
+               in db.session.query(Presenca.aluno_id, func.count(Presenca.id))
+               .join(Aula)
+               .filter_by(professor_id=professor_id, unidade_id=unidade_id)
+               .group_by(Presenca.aluno_id).all()]
     return jsonify(results=results), 200
